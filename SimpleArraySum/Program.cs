3c845966769a028c1c3054b907fa776a5a543507
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-27 12:44:35 +0300

    Simple Array Sum

---
+++ SimpleArraySum/Program.cs
@@ -0,0 +1,36 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'simpleArraySum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY ar as parameter.
+     */
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        foreach (int a in ar)
+        {
+            sum += a;
+        }
+        return sum;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+        int arCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.simpleArraySum(ar);
+
+        Console.WriteLine(result);
+    }
+}
