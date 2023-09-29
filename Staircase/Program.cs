30740c15bcf66de99d0d3b7f9da77fb1d328d77d
Esmanur Mazlum <esmanurmazlumm@gmail.com>
2023-09-29 12:32:13 +0300

    Staircase

---
+++ Staircase/Program.cs
@@ -0,0 +1,28 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'staircase' function below.
+     *
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static void staircase(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("".PadRight(i+1,'#').PadLeft(n));
+        }
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.staircase(n);
+    }
+}
